8eef62dc4399984f38790791378402365a10737d
Thomas Billicsich <billicsich@gmx.at>
2018-04-05 16:40:12 +0200

    Added logo

--- src/MainView.js
+++ src/MainView.js
@@ -3,32 +3,30 @@ import { Layout, Menu, Row, Col } from 'antd';
 import NotificationForm from './NotificationForm';
 import RequestForm from './RequestForm';
 import participants from './participants';
+import logo from './logo.png';
 
 const { Header, Footer, Content } = Layout;
+
 
 
 export default function MainView(props) {
   return (
     <Layout>
       <Header>
-        <Menu
-          theme="dark"
-          mode="horizontal"
-          defaultSelectedKeys={['2']}
-          style={{ lineHeight: '64px' }}
-        >
-        </Menu>
+        <img className="App-logo" src={logo}/>
       </Header>
-      <Content style={{ padding: '0 50px' }}>
-        <Row>
-          <Col span={6}>
-            <NotificationForm meter={participants()["meter1"]}/>
-          </Col>
-          <Col span={6}>
-            <RequestForm meter={participants()["meter1"]} />
-          </Col>
-        </Row>
-      </Content>
+      <Layout>
+        <Content style={{ padding: '0 50px' }}>
+          <Row>
+            <Col span={6} offset={3}>
+              <NotificationForm meter={participants()["meter1"]} />
+            </Col>
+            <Col span={6} offset={3}>
+              <RequestForm meter={participants()["meter1"]} />
+            </Col>
+          </Row>
+        </Content>
+      </Layout>
       <Footer>Made with <span role="img" aria-label="fun">😂</span> and <span role="img" aria-label="love">🧡</span> by Netpower</Footer>
     </Layout>
   );
