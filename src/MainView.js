1caaca5d29a5b1598e61bdbd4a451180900af24b
Thomas Billicsich <billicsich@gmx.at>
2018-04-05 12:24:38 +0200

    Added some UI elements to the MainView

--- src/MainView.js
+++ src/MainView.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import { Row, Col } from 'antd';
+import { Layout, Menu, Row, Col } from 'antd';
+import { Button, Input } from 'antd';
+const { Header, Footer, Sider, Content } = Layout;
 
 export default function MainView(props) {
-  return (<Row>
-    <Col span={6}>
-      <h1>Hello, {props.ame}</h1>
-    </Col>
-    <Col span={6}>
-      <h1>Hello, {props.name}</h1>
-    </Col>
-  </Row>);
+  return (<Layout>
+    <Header>
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['2']}
+        style={{ lineHeight: '64px' }}
+      >
+        <Menu.Item key="1">nav 1</Menu.Item>
+        <Menu.Item key="2">nav 2</Menu.Item>
+        <Menu.Item key="3">nav 3</Menu.Item>
+      </Menu>
+    </Header>
+    <Content style={{ padding: '0 50px' }}>
+      <Row>
+        <Col span={6}>
+          <h1>Hello, {props.ame}</h1>
+          <Button type="primary">Primary</Button>
+          <Input placeholder="Basic usage" />
+        </Col>
+        <Col span={6}>
+          <h1>Hello, {props.name}</h1>
+        </Col>
+      </Row>
+    </Content>
+    <Footer>Footer</Footer>
+  </Layout>
+  );
 }
